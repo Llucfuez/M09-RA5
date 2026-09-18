2cdeae34942c2b631cee78870e7fbca029c6b5e9
Llucfuez <elraptorgame06@gmail.com>
2026-09-18 16:43:30 +0200

    Codifica i descodifica fets

---
+++ RotX.java
@@ -0,0 +1,102 @@
+public class RotX {
+    
+    char[] majuscules = {
+    'A', 'Á', 'À', 'B', 'C', 'Ç', 'D', 'E', 'É', 'È',
+    'F', 'G', 'H', 'I', 'Í', 'Ì', 'Ï', 'J', 'K', 'L', 'M', 'N',
+    'Ñ', 'O', 'Ó', 'Ò', 'P', 'Q', 'R', 'S', 'T', 'U',
+    'Ú', 'Ù', 'Ü', 'V', 'W', 'X', 'Y', 'Z'
+    };
+
+    char[] minuscules = {
+    'a', 'á', 'à', 'b', 'c', 'ç', 'd', 'e', 'é', 'è',
+    'f', 'g', 'h', 'i', 'í', 'ì', 'ï', 'j', 'k', 'l', 'm', 'n',
+    'ñ', 'o', 'ó', 'ò', 'p', 'q', 'r', 's', 't', 'u',
+    'ú', 'ù', 'ü', 'v', 'w', 'x', 'y', 'z'
+    };
+
+    public StringBuffer xifraRotX(String cadena, int desplaçament){
+        
+        char caracter;
+        char nouValor;
+        StringBuffer xifrat = new StringBuffer();
+
+        for (int i = 0; i < cadena.length(); i++){
+            caracter = cadena.charAt(i);
+
+            int posicio = trobaPosicio(caracter);
+
+            if(posicio == -1){
+                xifrat.append(caracter);
+            } else {
+                boolean esMajuscula = Character.isUpperCase(caracter);
+                nouValor = asignaNouValor(posicio, desplaçament, esMajuscula, true);
+                xifrat.append(nouValor);        
+            }
+
+        }
+        return xifrat;
+    }
+
+    public StringBuffer desxifraRotX(String cadena, int desplaçament){ //Revisar
+        char caracter;
+        char nouValor;
+        StringBuffer desxifrat = new StringBuffer();
+
+        for (int i = 0; i < cadena.length(); i++){
+            caracter = cadena.charAt(i);
+
+            int posicio = trobaPosicio(caracter);
+
+            if(posicio == -1){
+                desxifrat.append(caracter);
+            } else {
+                boolean esMajuscula = Character.isUpperCase(caracter);
+                nouValor = asignaNouValor(posicio, desplaçament, esMajuscula, false);
+                desxifrat.append(nouValor);        
+            }
+
+        }
+        return desxifrat;
+
+    }
+
+    public int trobaPosicio(char caracter){    
+        for(int i = 0; i < minuscules.length; i++){
+            if(caracter == minuscules[i]){
+                return i;
+            }
+        }
+        
+        for(int i = 0; i < majuscules.length; i++){
+            if(caracter == majuscules[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public char asignaNouValor(int posicio, int desplaçament, boolean esMajuscula, boolean xifra){
+        int valor;
+        if (xifra){
+            valor = (posicio + desplaçament) % minuscules.length;  
+        } else {
+            valor = (posicio - desplaçament + majuscules.length) % minuscules.length;
+        }
+
+        if (esMajuscula){
+            return majuscules[valor];
+        } else {
+            return minuscules[valor];     
+        }
+    }
+
+    public static void main(String[] args){
+        RotX rotX = new RotX();
+
+        
+        
+        
+        
+    }
+    
+}
